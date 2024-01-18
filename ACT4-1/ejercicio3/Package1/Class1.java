c02f74f2b979bed681aac266921fc296578313ab
danugo50 <156311123+danugo50@users.noreply.github.com>
2024-01-18 14:08:25 +0100

    Tarea de entornos clases

---
+++ ACT4-1/ejercicio3/Package1/Class1.java
@@ -0,0 +1,42 @@
+package Package1;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Class1 {
+
+    /**
+     * Default constructor
+     */
+    public Class1() {
+    }
+
+    /**
+     * 
+     */
+    public String nombre;
+
+    /**
+     * 
+     */
+    public String color;
+
+    /**
+     * 
+     */
+    public boolean aptoPilotar;
+
+    /**
+     * @param x 
+     * @param y 
+     * @return
+     */
+    public float solucionarEcuacion(float x, float y) {
+        // TODO implement here
+        return 0.0f;
+    }
+
+}
---
+++ ACT4-1/ejercicio3/Package1/idolo.java
@@ -0,0 +1,22 @@
+package Package1;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class idolo {
+
+    /**
+     * Default constructor
+     */
+    public idolo() {
+    }
+
+    /**
+     * 
+     */
+    public String nombre;
+
+}
